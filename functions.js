6ad7a58909fb7f6cfa2be579c7e2ef3646b3d9b9
Chad <chadjcampbell@gmail.com>
2022-10-11 15:27:24 -0400

    fixed parentheses on tests, first function test green

--- functions.js
+++ functions.js
@@ -1,4 +1,6 @@
-function capitalize(string) {}
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function reverseString(string) {}
 
--- functions.test.js
+++ functions.test.js
@@ -9,31 +9,29 @@ import {
 // Test names would not be so silly in a real environment :)
 
 test("Capitalize the string please", () => {
-  expect(capitalize("joe").toBe("Joe"));
+  expect(capitalize("joe")).toBe("Joe");
 });
 
 test("Flip switch it and reverse it", () => {
-  expect(reverseString("flip this").toBe("siht pilf"));
+  expect(reverseString("flip this")).toBe("siht pilf");
 });
 
 test("Calculizor engage", () => {
-  expect(calc.add(1, 1).toBe(2));
-  expect(calc.subtract(5, 1).toBe(4));
-  expect(calc.divide(8, 4).toBeCloseTo(2));
-  expect(calc.multiply(3, 5).toBeCloseTo(15));
+  expect(calc.add(1, 1)).toBe(2);
+  expect(calc.subtract(5, 1)).toBe(4);
+  expect(calc.divide(8, 4)).toBeCloseTo(2);
+  expect(calc.multiply(3, 5)).toBeCloseTo(15);
 });
 
 test("Now it's a secret", () => {
-  expect(caesarCipher("Attack at dawn", 5).toBe("Fyyfhp fy ifbs"));
+  expect(caesarCipher("Attack at dawn", 5)).toBe("Fyyfhp fy ifbs");
 });
 
 test("Analysis paralysis, woof!", () => {
-  expect(
-    analyzeArray([1, 8, 3, 4, 2, 6]).toEqual({
-      average: 4,
-      min: 1,
-      max: 8,
-      length: 6,
-    })
-  );
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
