94ec5654bc72755c9aaed5c6f44a56fe5b49584b
Chad <chadjcampbell@gmail.com>
2022-10-11 15:43:57 -0400

    flip and calculator function tests now green

--- functions.js
+++ functions.js
@@ -2,18 +2,28 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function reverseString(string) {}
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
 
 class Calculator {
   constructor() {}
 
-  add(x, y) {}
+  add(x, y) {
+    return x + y;
+  }
 
-  subtract(x, y) {}
+  subtract(x, y) {
+    return x - y;
+  }
 
-  divide(x, y) {}
+  divide(x, y) {
+    return x / y;
+  }
 
-  multiply(x, y) {}
+  multiply(x, y) {
+    return x * y;
+  }
 }
 
 const calc = new Calculator();
