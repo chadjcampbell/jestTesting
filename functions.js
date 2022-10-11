5449553d4e9b23abbdd7c5f8e7dc17ad547a5651
Chad <chadjcampbell@gmail.com>
2022-10-11 16:11:22 -0400

    4 out of 5 function tests now green, only analyzeArray remains

--- functions.js
+++ functions.js
@@ -28,7 +28,25 @@ class Calculator {
 
 const calc = new Calculator();
 
-function caesarCipher(string, shift) {}
+function caesarCipher(string, shift) {
+  if (shift < 0) {
+    return caesarCipher(string, shift + 26);
+  }
+  let output = "";
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (char.match(/[a-z]/i)) {
+      let code = string.charCodeAt(i);
+      if (code >= 65 && code <= 90) {
+        char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+      } else if (code >= 97 && code <= 122) {
+        char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+      }
+    }
+    output += char;
+  }
+  return output;
+}
 
 function analyzeArray(array) {}
 
